bf48fe2d008d578b9ca957929d2dddef697f7416
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-05 14:26:09 -0500

    a couple small tests

--- ckanext/webhooks/tests/test.py
+++ ckanext/webhooks/tests/test.py
@@ -1,18 +1,35 @@
-"""Tests for plugin.py."""
-import ckanext.webhooks.plugin as plugin
 import nose
+import pylons
+import ckanapi
+import paste.fixture
 import ckan.tests as tests
+import sqlalchemy.orm as orm
 
-class TestWebhooks(object)
+import ckan.plugins as p
+import ckanext.webhooks.plugin as plugin
+import ckan.config.middleware as middleware
 
-    def test_webhook_create():
-        pass
+from pylons import config
 
-    def test_webhook_show():
-        pass
+class TestWebhooks(tests.WsgiAppCase):
 
-    def test_webhook_delete():
-        pass
+    @classmethod
+    def setup_class(cls):
+        wsgiapp = middleware.make_app(config['global_conf'], **config)
+        cls.app = paste.fixture.TestApp(wsgiapp)
 
-    def test_webhook_receive():
+    def test_webhook_actions(self):
+        ckan = ckanapi.LocalCKAN()
+        callback = 'http://example.com/webhook_callback'
+        
+        hook = ckan.action.webhook_create(topic='dataset/create', address=callback)
+        assert hook
+
+        show = ckan.action.webhook_show(id=hook)
+        assert show['id'] == hook
+
+        delete = ckan.action.webhook_delete(id=hook)
+        assert delete == hook
+
+    def test_webhook_receive(self):
         pass
