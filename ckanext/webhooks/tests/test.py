94fe969e70062ffc869cbedf14a7fa044d6f6e1a
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-05 13:36:35 -0500

    test stubs

---
+++ ckanext/webhooks/tests/test.py
@@ -0,0 +1,18 @@
+"""Tests for plugin.py."""
+import ckanext.webhooks.plugin as plugin
+import nose
+import ckan.tests as tests
+
+class TestWebhooks(object)
+
+    def test_webhook_create():
+        pass
+
+    def test_webhook_show():
+        pass
+
+    def test_webhook_delete():
+        pass
+
+    def test_webhook_receive():
+        pass
