74639736c4416cd984e130be5d73a64287cb5724
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-05 11:12:25 -0500

    validator schema

--- ckanext/webhooks/actions.py
+++ ckanext/webhooks/actions.py
@@ -1,7 +1,23 @@
 import db
+import logging
+
+from ckan.plugins.toolkit import get_validator
+import ckan.lib.navl.dictization_functions as df
+
+log = logging.getLogger(__name__)
+
+schema = {
+    'id': [get_validator('ignore_empty'), unicode],
+    'address': [get_validator('not_empty'), unicode],
+    'topic': [get_validator('not_empty'), unicode],
+    'user_id': [get_validator('ignore_missing'), unicode],
+    'created_at': [get_validator('ignore_missing'), get_validator('isodate')]
+}
 
 def webhook_create(context, data_dict):
-    pass
+    log.info(data_dict)
+
+    data, errors = df.validate(data_dict, schema, context)
 
 def webhook_delete(context, data_dict):
     pass
