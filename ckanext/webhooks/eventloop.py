1cd5477cf212385b63e34a21ee51de31d8390dd8
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-07 17:04:03 -0500

    we need to use an async http client, so as to not block the tornado ioloop

--- ckanext/webhooks/eventloop.py
+++ ckanext/webhooks/eventloop.py
@@ -1,10 +1,10 @@
 import json
 import logging
-import requests
 import tornado.web
 import tornado.ioloop
 
 from tornado.options import define, options, parse_command_line
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 
 define("port", default=8765, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
@@ -12,19 +12,25 @@ define("debug", default=True, help="run in debug mode")
 class WebhookReceiver(tornado.web.RequestHandler):
 
     def post(self):
-        self.finish()
-
         body = json.loads(self.request.body)
         url = body['address']
 
-        fire_webhook(url, self.request.body)
+        httpclient = AsyncHTTPClient()
+        request = HTTPRequest(
+            url = url,
+            method = 'POST',
+            headers = {
+                'Content-Type': 'application/json'
+            },
+            body = data
+        )
 
-def fire_webhook(url, data):
-    requests.post(url, headers = {
-            'Content-Type': 'application/json'
-        },
-        data = data
-    )
+        httpclient.fetch(request, request_handler)
+
+        self.finish()
+
+    def request_handler(self):
+        pass
 
 def main():
     parse_command_line()
